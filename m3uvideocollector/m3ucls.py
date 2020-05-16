77079eb39117a9b354f433ff915d5802a6d54711
fch-luck <fch.luck@qq.com>
2020-05-16 16:06:36 +0800

    添加注释；改写方法从视频播放页响应中自动提取m3u8链接并下载文件和解析

--- m3uvideocollector/m3ucls.py
+++ m3uvideocollector/m3ucls.py
@@ -13,6 +13,7 @@ import time
 import re
 from requests_html import HTMLSession  # 用于下载文件
 from browsermobproxy import Server
+from make_time_formated import *
 # browsermobproxy 是基于LittleProxy的的代理服务，启用时作为，作为Selenium浏览器的标准代理，抓取请求和返回内容
 # 需下载脚本文件配合用https://github.com/lightbody/browsermob-proxy/releases
 
@@ -28,11 +29,10 @@ class M3uCllector(object):
 
         self.session = HTMLSession()
 
-        self.index_m3u8_url = None
-        self.indexm3u8 = None
+        self.m3u8_urls = None
         self.ts_base_url = None  # ts文件存放路径的base URL
         self.ts_urls = []
-        self.tss = dict()
+        self.tsdata = dict()
 
         self.config_driver()  # 初始化webdriver和browsermob_proxy服务器
 
@@ -48,7 +48,12 @@ class M3uCllector(object):
         chrome_options.add_argument('--no-sandbox')
         self.driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=self.webdriverpath)
 
-    def get_index_m3u8_url(self, videoplayurl):
+    def get_m3u8_urls(self, videoplayurl):
+        """
+        监听HTTP响应结果，找出.m3u8链接
+        :param videoplayurl: 视频播放页面URL
+        :return: m3u8链接（列表）
+        """
         self.proxy.new_har("demo", options={'captureHeaders': True, 'captureContent': True})  # 创建HAR文件
         # HAR是记录http请求发出直到收到完整的响应所发生的http交互细节（url、响应时间等等）的标准化工具
         self.driver.get(videoplayurl)
@@ -58,73 +63,52 @@ class M3uCllector(object):
         # f.write(har)
         # time.sleep(1)
         # f.close()
+
+        # html = self.driver.page_source
+        # f = open("page_source.html", "w", encoding="utf-8")
+        # f.write(html)
+        # time.sleep(1)
+        # f.close()
+
         entries = har["log"]["entries"]  # 提取http请求和响应条目(结构参看har.js文件)
-        indexm3u8_url = None
-
+        m3u8_urls = []
         for entry in entries:
-            if "index.m3u8" in entry["request"]["url"]:
+            if entry["request"]["url"].endswith(".m3u8"):  # 提取.m3u8链接
                 if entry["response"]["status"] == 200:
-                    indexm3u8_url = entry["request"]["url"]
-                    print("index.m3u8 found!")
-                    break
-        if not indexm3u8_url:
-            print("index.m3u8 no found!")
-        self.index_m3u8_url = indexm3u8_url
+                    print("m3u8 url found: " + entry["request"]["url"])
+                    m3u8_urls.append(entry["request"]["url"])
+        if not m3u8_urls:
+            print("m3u8 url no found!")
+        self.m3u8_urls = m3u8_urls
+        return m3u8_urls
 
-    def get_index_m3u8(self):
-        indexm3u8 = None
-        ts_base_url = None
-        if self.index_m3u8_url:
-            response = self.session.get(self.index_m3u8_url)  # index.m3u8文件下载
-            indexm3u8 = response.html.html
-            print("index.m3u8 url: " + self.index_m3u8_url)
-            print("index.m3u8 文件内容: \n" + indexm3u8)
-            m = re.search("\S+.com", self.index_m3u8_url)  # base url https://605ziyuan.com
-            ts_base_url = m.group()
+    def get_ts_urls(self, m3u8urls: list, tsbaseurl=None):
+        ts_base_url = tsbaseurl
+        ts_urls = []
+        if not m3u8urls:
+            print("please input a list of urls")
+        else:
+            for m3u8url in m3u8urls:
+                response = self.session.get(m3u8url)  # index.m3u8文件下载
+                m3u8 = response.html.html
+                print("m3u8 url: " + m3u8url)
+                print("parsing .m3u8: \n" + m3u8)
+                lines = m3u8.split("\n")
+                for line in lines:
+                    if line.endswith(".ts"):
+                        print(line)
+                        ts_urls.append(line)
+                if ts_urls:
+                    if not ts_base_url:
+                        m = re.search("\S+.com", m3u8url)  # 截取.ts文件链接头base url https://605ziyuan.com
+                        ts_base_url = m.group()
+                else:
+                    print(".m3u8 file parsed, but fail to get url of ts files")
+        if ts_base_url and ts_urls:
+            ts_urls = [ts_base_url + url for url in ts_urls]  # 合并成完整ts URL
         self.ts_base_url = ts_base_url
-        self.indexm3u8 = indexm3u8
-
-    # html = self.driver.page_source
-    # f = open("demo.html", "w", encoding="utf-8")
-    # f.write(html)
-    # time.sleep(1)
-    # f.close()
-
-    def get_ts_m3u8_urls(self):
-        global ts_url
-        if self.ts_base_url and self.indexm3u8:
-            lines = self.indexm3u8.split("\n")
-            for line in lines:
-                if ".m3u8" in line:
-                    if ("https://" in line) or ("http://" in line):
-                        tsm3u8_url = line
-                    else:
-                        m = re.search("/\S+.m3u8", line)  # relative path /ppvod/RuIp5mQv.m3u8
-                        if not m:
-                            print("parse index.m3u8 failed, fail to get url of ts links m3u8")
-                            break
-                        else:
-                            tsm3u8_url = self.ts_base_url + m.group()
-                            print(".m3u8 found!")
-                    response = self.session.get(tsm3u8_url)  # 包含ts文件链接的.m3u8文件下载
-                    tsm3u8 = response.html.html
-                    print(".m3u8 url: " + tsm3u8_url)
-                    print(".m3u8 文件内容: \n" + tsm3u8)
-                    lines = tsm3u8.split("\n")
-
-                    for line in lines:
-                        ts_url = None
-                        if ".ts" in line:
-                            if ("https://" in line) or ("http://" in line):
-                                ts_url = line
-                            else:
-                                m = re.search("/\S+.ts", line)  # relative path /20180506/zDOacWsg/800kb/hls/rzEv2825000.ts
-                                if not m:
-                                    print("parse .m3u8 failed, fail to get url of ts files")
-                                else:
-                                    ts_url = self.ts_base_url + m.group()
-                            print(ts_url)
-                            self.ts_urls.append(ts_url)
+        self.ts_urls = ts_urls
+        return ts_urls
 
     def download_ts_data(self, tsurl):
         ts_name = None
@@ -138,33 +122,44 @@ class M3uCllector(object):
             # print(ts_data)
         return ts_name, ts_data
 
-    def save_ts_file(self, ts_name, ts_data):
-        if ts_name and ts_data:
+    def save_ts_file(self, tssavepath, tsdata):
+        if tssavepath and tsdata:
             try:
-                file = open(ts_name, "wb", )
-                file.write(ts_data)
+                file = open(tssavepath, "wb", )
+                file.write(tsdata)
                 file.close()
-                print("ts file {} saved.".format(ts_name))
+                print("file {} saved.".format(tssavepath))
             except Exception as e:
-                print("save ts file failed. detail: {}".format(e))
+                print("save file failed. detail: {}".format(e))
 
-    def download_ts_files(self):
-        if self.ts_urls:
-            tsurls = sorted(self.ts_urls)
-            for tsurl in tsurls:
+    def download_ts_files(self, tsurls=None, savepath=""):
+        if not tsurls:
+            ts_urls = self.ts_urls
+        else:
+            ts_urls = tsurls
+        if ts_urls:
+            for tsurl in ts_urls:
                 if tsurl:
                     ts_name, ts_data = self.download_ts_data(tsurl)
-                    self.tss[ts_name] = ts_data
+                    self.tsdata[ts_name] = ts_data
                     time.sleep(0.1)
             time.sleep(1)
             tsdatas = []
-            for k in sorted(self.tss.keys()):
-                tsdatas.append(self.tss[k])
+            for k in sorted(self.tsdata.keys()):
+                tsdatas.append(self.tsdata[k])
             data = b''.join(tsdatas)
-            self.save_ts_file("video.mp4", data)
+            if not savepath:
+                sp = nowtimestr("%Y%m%d%H%M%S") + ".mp4"
+            else:
+                sp = savepath + "\\" + nowtimestr("%Y%m%d%H%M%S") + ".mp4"
+            self.save_ts_file(sp, data)
 
-
-    def merge_ts_files(self):
+    def merge_ts_files(self, tsfilespath: str):
+        """
+        merge ts files to a video
+        :param tsspath: path that contains several ts files
+        :return:
+        """
         pass
 
 
@@ -173,11 +168,9 @@ if __name__ == "__main__":
     browsermob_proxy_bat_path = r"E:\MyWorkPlace\spiders\browsermob-proxy\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat"
     video_play_url = "http://www.zd518.cn/vod-play-id-142752-src-1-num-1.html"
     collector = M3uCllector(webdriver_path, browsermob_proxy_bat_path)
-    collector.get_index_m3u8_url(video_play_url)
-    collector.get_index_m3u8()
-    collector.get_ts_m3u8_urls()
-    collector.download_ts_files()
-    # tsname, tsdata = collector.download_ts_data(collector.ts_urls[0])
-    # print("tsname, tsdata")
-    # collector.save_ts_file(tsname, tsdata)
+    # m3u8_urls = collector.get_m3u8_urls(video_play_url)
+    m3u8_urls = ["https://2.ddyunbo.com/20191219/2V4EJdvv/800kb/hls/index.m3u8"]
+    ts_urls = collector.get_ts_urls(m3u8_urls)
+    output = r"E:\MyWorkPlace\spiders\m3uvideocollector\download"
+    collector.download_ts_files(ts_urls, output)
 
